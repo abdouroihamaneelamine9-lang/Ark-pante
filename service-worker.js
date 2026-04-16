afeb002c9c7c97cea833328d47a887ea520fb965
abdouroihamaneelamine9-lang <abdouroihamaneelamine9@gmail.com>
2026-04-16 16:22:42 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "routine-cache-v1";
+const CACHE_NAME = "routine-cache-v2";
 
 const urlsToCache = [
   "/",
@@ -6,18 +6,40 @@ const urlsToCache = [
   "/manifest.json"
 ];
 
+// INSTALLATION
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
+// ACTIVER (nettoyage ancien cache)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// FETCH = fonctionnement hors ligne
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request)
       .then(response => {
-        return response || fetch(event.request);
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
       })
   );
 });
